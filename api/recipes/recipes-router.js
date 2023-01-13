3c52d1fcf767c82136983e3395d421a3258ad629
Kyle Andrus <kyleandrus3@gmail.com>
2023-01-12 17:38:42 -0700

    set up get response in recipes-router.js and getRecipeById function in recipes-model.js

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,6 +1,15 @@
 const router = require('express').Router()
+const Recipe = require('./recipes-model')
 
-router.use('*', (req, res, next) => {
+router.get('/:recipe_id', (req, res, next) => {
+    Recipe.getRecipeById(req.params.recipe_id)
+    .then(recipe => {
+        res.status(200).json(recipe)
+    })
+    .catch(next)
+})
+
+router.use('*', (req, res) => {
     res.json({ api: 'up' })
 })
 
