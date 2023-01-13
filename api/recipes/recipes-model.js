779fdf302d8eab7af54cd566ad558b7ef48bdc61
Kyle Andrus <kyleandrus3@gmail.com>
2023-01-13 12:35:49 -0700

    modify return of data structure from getRecipeById in recipes-model.js

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -7,14 +7,27 @@ left join ingredients as i on si.ingredient_id=i.ingredient_id
 where r.recipe_id = 1
 */
 async function getRecipeById(recipe_id) {
-    const uniqueRecipe = db('recipes as r')
+    const uniqueRecipe = await db('recipes as r')
     .select('r.recipe_name', 's.step_id', 's.step_number', 's.step_instructions', 'i.ingredient_id', 'i.ingredient_name', 'si.quantity')
     .leftJoin('steps as s', 'r.recipe_id','s.recipe_id')
     .leftJoin('step_ingredients as si', 'si.step_id', 's.step_id')
     .leftJoin('ingredients as i', 'si.ingredient_id', 'i.ingredient_id')
     .where('r.recipe_id', recipe_id)
 
-    return uniqueRecipe
+    const recipes = {
+        recipe_name: uniqueRecipe[0].recipe_name,
+        steps: uniqueRecipe.reduce((acc, row) => {
+            if(!row.ingredient_id) {
+            acc.push({ step_number: row.step_number, step_instructions: row.step_instructions }) 
+            } else {
+                acc.push({ step_number: row.step_number, step_instructions: row.step_instructions, ingredients: [
+                    { ingredient_id: row.ingredient_id, ingredient_name: row.ingredient_name}
+                ] })
+            }
+            return acc        
+        }, [])
+    }
+    return recipes
 }
 
 module.exports = {
